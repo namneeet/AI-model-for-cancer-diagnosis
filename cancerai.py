feef07acc21b7e5eda134b8dff9ac65de96a925e
namneeeet <139070475+namneeet@users.noreply.github.com>
2024-08-05 15:41:09 -0300

    Update cancerai.py

--- cancerai.py
+++ cancerai.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
-# set system encoding to utf-8
+# s et system encoding to utf-8
 sys.stdout.reconfigure(encoding='utf-8')
 
 dataset = pd.read_csv('cancer.csv')
@@ -16,7 +16,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Dense(256, input_shape=x_train.shape[1:], activation="sigmoid"))
 model.add(tf.keras.layers.Dense(256, activation="sigmoid"))
-model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
+model.add(tf.keras.layers.Dense(1, activation="sigmoid")) 
 
 model.compile(optimizer='adam', loss="binary_crossentropy", metrics=['accuracy'])
 
